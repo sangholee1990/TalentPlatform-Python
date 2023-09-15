87f680b6ed34f7279d0f2d824fcad067a2d9db92
해솔Haesol <sangho.lee.1990@gmail.com>
2023-09-15 14:57:07 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/talentPlatform/unitSys/TalentPlatform-LSH0468.py
@@ -0,0 +1,372 @@
+# -*- coding: utf-8 -*-
+import argparse
+import glob
+import logging
+import logging.handlers
+import os
+import platform
+import sys
+import traceback
+import warnings
+from datetime import datetime
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import json
+
+# =================================================
+# 사용자 매뉴얼
+# =================================================
+# [소스 코드의 실행 순서]
+# 1. 초기 설정 : 폰트 설정
+# 2. 유틸리티 함수 : 초기화 함수 (로그 설정, 초기 변수, 초기 전달인자 설정) 또는 자주 사용하는 함수
+# 3. 주 프로그램 :부 프로그램을 호출
+# 4. 부 프로그램 : 자료 처리를 위한 클래스로서 내부 함수 (초기 변수, 비즈니스 로직, 수행 프로그램 설정)
+# 4.1. 환경 변수 설정 (로그 설정) : 로그 기록을 위한 설정 정보 읽기
+# 4.2. 환경 변수 설정 (초기 변수) : 입력 경로 (inpPath) 및 출력 경로 (outPath) 등을 설정
+# 4.3. 초기 변수 (Argument, Option) 설정 : 파이썬 실행 시 전달인자 설정 (pyhton3 *.py argv1 argv2 argv3 ...)
+# 4.4. 비즈니스 로직 수행 : 단위 시스템 (unit 파일명)으로 관리 또는 비즈니스 로직 구현
+
+# =================================================
+# 1. 초기 설정
+# =================================================
+warnings.filterwarnings("ignore")
+
+plt.rc('font', family='Malgun Gothic')
+plt.rc('axes', unicode_minus=False)
+# sns.set(font="Malgun Gothic", rc={"axes.unicode_minus": False}, style='darkgrid')
+
+# 그래프에서 마이너스 글꼴 깨지는 문제에 대한 대처
+mpl.rcParams['axes.unicode_minus'] = False
+
+
+# =================================================
+# 2. 유틸리티 함수
+# =================================================
+# 로그 설정
+def initLog(env=None, contextPath=None, prjName=None):
+    if env is None: env = 'local'
+    if contextPath is None: contextPath = os.getcwd()
+    if prjName is None: prjName = 'test'
+
+    saveLogFile = "{}/{}_{}_{}_{}_{}_{}.log".format(
+        contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'log', prjName)
+        , platform.system()
+        , platform.machine()
+        , platform.architecture()[0]
+        , platform.node()
+        , prjName
+        , datetime.now().strftime("%Y%m%d")
+    )
+
+    os.makedirs(os.path.dirname(saveLogFile), exist_ok=True)
+
+    # logger instance 생성
+    log = logging.getLogger(prjName)
+
+    if len(log.handlers) > 0:
+        return log
+
+    # format 생성
+    format = logging.Formatter('%(asctime)s [%(name)s | %(lineno)d | %(filename)s] [%(levelname)-5.5s] %(message)s')
+
+    # handler 생성
+    streamHandler = logging.StreamHandler()
+    fileHandler = logging.FileHandler(saveLogFile)
+
+    # logger instance에 format 설정
+    streamHandler.setFormatter(format)
+    fileHandler.setFormatter(format)
+
+    # logger instance에 handler 설정
+    log.addHandler(streamHandler)
+    log.addHandler(fileHandler)
+
+    # logger instance로 log 기록
+    log.setLevel(level=logging.INFO)
+
+    return log
+
+
+#  초기 변수 설정
+def initGlobalVar(env=None, contextPath=None, prjName=None):
+    if env is None: env = 'local'
+    if contextPath is None: contextPath = os.getcwd()
+    if prjName is None: prjName = 'test'
+
+    # 환경 변수 (local, 그 외)에 따라 전역 변수 (입력 자료, 출력 자료 등)를 동적으로 설정
+    # 즉 local의 경우 현재 작업 경로 (contextPath)를 기준으로 설정
+    # 그 외의 경우 contextPath/resources/input/prjName와 같은 동적으로 구성
+    globalVar = {
+        'prjName': prjName
+        , 'sysOs': platform.system()
+        , 'contextPath': contextPath
+        , 'resPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources')
+        , 'cfgPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config')
+        , 'inpPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'input', prjName)
+        , 'figPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'fig', prjName)
+        , 'outPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'output', prjName)
+        , 'movPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'movie', prjName)
+        , 'logPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'log', prjName)
+        , 'mapPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'mapInfo')
+        , 'sysPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config', 'system.cfg')
+        , 'seleniumPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config', 'selenium')
+        , 'fontPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config', 'fontInfo')
+    }
+
+    return globalVar
+
+
+#  초기 전달인자 설정
+def initArgument(globalVar, inParams):
+    # 원도우 또는 맥 환경
+    if globalVar['sysOs'] in 'Windows' or globalVar['sysOs'] in 'Darwin':
+        inParInfo = inParams
+
+    # 리눅스 환경
+    if globalVar['sysOs'] in 'Linux':
+        parser = argparse.ArgumentParser()
+
+        for i, argv in enumerate(sys.argv[1:]):
+            if not argv.__contains__('--'): continue
+            parser.add_argument(argv)
+
+        inParInfo = vars(parser.parse_args())
+
+    log.info("[CHECK] inParInfo : {}".format(inParInfo))
+
+    for key, val in inParInfo.items():
+        if val is None: continue
+        # 전역 변수에 할당
+        globalVar[key] = val
+
+    # 전역 변수
+    for key, val in globalVar.items():
+        if env not in 'local' and key.__contains__('Path') and env and not os.path.exists(val):
+            os.makedirs(val)
+
+        globalVar[key] = val.replace('\\', '/')
+
+        log.info("[CHECK] {} : {}".format(key, val))
+
+        # self 변수에 할당
+        # setattr(self, key, val)
+
+    return globalVar
+
+
+# ================================================
+# 4. 부 프로그램
+# ================================================
+class DtaProcess(object):
+    # ================================================
+    # 요구사항
+    # ================================================
+    # Python을 이용한 ECMWF 및 GFS 예보모델 전처리 결과를 토대로 NetCDF 생산
+
+    # ================================================================================================
+    # 환경변수 설정
+    # ================================================================================================
+    global env, contextPath, prjName, serviceName, log, globalVar
+
+    # env = 'local'  # 로컬 : 원도우 환경, 작업환경 (현재 소스 코드 환경 시 .) 설정
+    env = 'dev'  # 개발 : 원도우 환경, 작업환경 (사용자 환경 시 contextPath) 설정
+    # env = 'oper'  # 운영 : 리눅스 환경, 작업환경 (사용자 환경 시 contextPath) 설정
+
+    if (platform.system() == 'Windows'):
+        contextPath = os.getcwd() if env in 'local' else 'E:/04. TalentPlatform/Github/TalentPlatform-Python'
+    else:
+        contextPath = os.getcwd() if env in 'local' else '/SYSTEMS/PROG/PYTHON/PyCharm'
+
+    prjName = 'test'
+    serviceName = 'LSH0468'
+
+    # 4.1. 환경 변수 설정 (로그 설정)
+    log = initLog(env, contextPath, prjName)
+
+    # 4.2. 환경 변수 설정 (초기 변수)
+    globalVar = initGlobalVar(env, contextPath, prjName)
+
+    # ================================================================================================
+    # 4.3. 초기 변수 (Argument, Option) 설정
+    # ================================================================================================
+    def __init__(self, inParams):
+
+        log.info("[START] __init__ : {}".format("init"))
+
+        try:
+            # 초기 전달인자 설정 (파이썬 실행 시)
+            # pyhton3 *.py argv1 argv2 argv3 ...
+            initArgument(globalVar, inParams)
+
+        except Exception as e:
+            log.error("Exception : {}".format(e))
+            raise e
+        finally:
+            log.info("[END] __init__ : {}".format("init"))
+
+    # ================================================================================================
+    # 4.4. 비즈니스 로직 수행
+    # ================================================================================================
+    def exec(self):
+
+        log.info('[START] {}'.format("exec"))
+
+        try:
+
+            if platform.system() == 'Windows':
+                pass
+            else:
+                globalVar['inpPath'] = '/DATA/INPUT'
+                globalVar['outPath'] = '/DATA/OUTPUT'
+                globalVar['figPath'] = '/DATA/FIG'
+                globalVar['updPath'] = '/DATA/CSV'
+
+            # 옵션 설정
+            sysOpt = {
+                # 수행 목록
+                'nameList': ['json']
+
+                # 모델 정보 : 파일 경로, 파일명
+                , 'nameInfo': {
+                    'json': {
+                        'filePath': '/DATA/INPUT/LSH0468'
+                        , 'fileName': 'land_register*.json'
+                    }
+                }
+            }
+
+
+            for i, nameType in enumerate(sysOpt['nameList']):
+                log.info(f'[CHECK] nameType : {nameType}')
+
+                nameInfo = sysOpt['nameInfo'].get(nameType)
+                if nameInfo is None: continue
+
+                inpFile = '{}/{}'.format(nameInfo['filePath'], nameInfo['fileName'])
+                fileList = sorted(glob.glob(inpFile))
+
+                if fileList is None or len(fileList) < 1:
+                    # log.error(f'inpFile : {inpFile} / 입력 자료를 확인해주세요')
+                    continue
+
+                for j, fileInfo in enumerate(fileList):
+                    log.info(f'')
+                    log.info(f'[CHECK] fileInfo : {fileInfo}')
+
+                    # 1. JSON 파일 읽기
+                    with open(fileInfo, 'r', encoding='utf-8') as f:
+                        data = json.load(f)
+                    log.info(f'[CHECK] data : {data}')
+
+                    # 결과를 저장할 dictionary 초기화
+                    result = {
+                        "고유번호": "" if data.get('고유번호') is None or len(data.get('고유번호')) < 1 else data["고유번호"],
+                        "토지소재": "" if data.get('토지소재') is None or len(data.get('토지소재')) < 1 else data["토지소재"],
+                        "지번": "" if data.get('지번') is None or len(data.get('지번')) < 1 else data["지번"],
+                        "지목": "" if data.get('토지표시') is None or len(data.get('토지표시')) < 1 else data["토지표시"][-1]["지목"],
+                        "면적": "" if data.get('토지표시') is None or len(data.get('토지표시')) < 1 else data["토지표시"][-1]["면적"],
+                        "공시지가": "" if data.get('개별공시지가') is None or len(data.get('개별공시지가')) < 1 else data["개별공시지가"][-1]["공시지가"],
+                        "변동일자": "",
+                        "소유자이름": "",
+                        "소유자주소": "",
+                        "소유자등록번호": "",
+                        "소유권변동일자": ""
+                    }
+
+                    # 5. '미등기'가 있는지 확인
+                    isNotReg = any(entry["변동원인"] == "미등기" for entry in data["소유자정보"])
+
+                    if isNotReg:
+                        # 미등기일 경우
+                        for ownerInfo in reversed(data["소유자정보"]):
+                            if ownerInfo["성명"]:
+                                result["소유자이름"] = ownerInfo["성명"]
+                                break
+
+                        for ownerInfo in reversed(data["소유자정보"]):
+                            if ownerInfo["주소"]:
+                                result["소유자주소"] = ownerInfo["주소"]
+                                break
+
+                        for ownerInfo in reversed(data["소유자정보"]):
+                            if ownerInfo["등록번호"]:
+                                result["소유자등록번호"] = ownerInfo["등록번호"]
+                                break
+
+                        result["변동일자"] = data["소유자정보"][-1]["변동일자"]
+                    else:
+                        # 미등기가 아닌 경우
+                        lastChangeDate = data["소유자정보"][-1]["변동일자"]
+                        for ownerInfo in data["소유자정보"]:
+                            if ownerInfo["변동일자"] == lastChangeDate:
+                                result["변동일자"] = ownerInfo["변동일자"]
+                                result["소유자이름"] = ownerInfo["성명"]
+                                result["소유자주소"] = ownerInfo["주소"]
+                                result["소유자등록번호"] = ownerInfo["등록번호"]
+
+                    # 4. 소유권변동일자
+                    ownerChangeDateList = [entry["변동일자"] for entry in data["소유자정보"] if "소유" in entry["변동원인"]]
+                    if ownerChangeDateList:
+                        result["소유권변동일자"] = max(ownerChangeDateList)
+
+                    log.info(f'[CHECK] result : {result}')
+
+
+
+                #
+                #     data = pd.read_csv(fileInfo)
+                #     data['anaDt'] = anaDt
+                #     data['forDt'] = forDt
+                #
+                #     dataL1 = data.drop(columns=['n'])
+                #
+                #     dataL2 = pd.concat([dataL2, dataL1], ignore_index=False)
+                #
+                # dataL3 = dataL2.set_index(['anaDt', 'forDt', 'Latitude', 'Longitude'])
+                # dataL4 = dataL3.to_xarray()
+                #
+                # # dataL4['gfs_Value'].isel(anaDt = 0, forDt = 0).plot()
+                # # plt.show()
+                #
+                # saveFilePath = os.path.dirname(fileInfo)
+                # saveFileName = os.path.basename(fileInfo)
+                # saveFileName = re.sub(r'(\d+h)', '{}', saveFileName, re.IGNORECASE).replace('.csv', '.nc')
+                #
+                # # NetCDF 자료 저장
+                # saveFile = f'{saveFilePath}/{saveFileName}'.format(len(dataL4['forDt']))
+                # os.makedirs(os.path.dirname(saveFile), exist_ok=True)
+                # dataL4.to_netcdf(saveFile)
+                # log.info(f'[CHECK] saveFile : {saveFile}')
+
+        except Exception as e:
+            log.error(f'Exception : {e}')
+
+        finally:
+            log.info('[END] {}'.format("exec"))
+
+# ================================================
+# 3. 주 프로그램
+# ================================================
+if __name__ == '__main__':
+
+    print('[START] {}'.format("main"))
+
+    try:
+
+        # 파이썬 실행 시 전달인자를 초기 환경변수 설정
+        inParams = {}
+
+        print("[CHECK] inParams : {}".format(inParams))
+
+        # 부 프로그램 호출
+        subDtaProcess = DtaProcess(inParams)
+
+        subDtaProcess.exec()
+
+    except Exception as e:
+        print(traceback.format_exc())
+        sys.exit(1)
+
+    finally:
+        print('[END] {}'.format("main"))
