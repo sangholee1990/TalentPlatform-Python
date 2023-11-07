052509a1ab56e6ce4542464952489460639efc07
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2023-11-07 17:50:53 +0900

    Merge remote-tracking branch 'origin/master'

--- src/talentPlatform/unitSys/TalentPlatform-LSH0442-Easy-ESACCI.py
+++ src/talentPlatform/unitSys/TalentPlatform-LSH0442-Easy-ESACCI.py
@@ -90,126 +90,156 @@ dtDayList = pd.date_range(start=dtSrtDate, end=dtEndDate, freq=Day(1))
 # 자료 처리
 # ****************************************************************************
 # 날짜 기준으로 반복문
-dataL3 = xr.Dataset()
-for dtDayIdx, dtDayInfo in enumerate(dtDayList):
-    # print(f'[CHECK] dtDayInfo : {dtDayInfo}')
-
-    # inpFile = '{}/{}'.format('/home/data/satellite/OCO3/OCO3_L2_Lite_SIF.10r', 'oco3_LtSIF_%y%m%d_B*_*.nc4')
-    inpFile = '{}/{}'.format( '/DATA/INPUT/LSH0442', '%Y%m%d-ESACCI-L3C_SNOW-SCFG-MODIS_TERRA-fv2.0.nc')
-    inpFileDate = dtDayInfo.strftime(inpFile)
-    fileList = sorted(glob.glob(inpFileDate))
-
-    if fileList is None or len(fileList) < 1: continue
-
-    for fileInfo in fileList:
-
-        print(f'[CHECK] fileInfo : {fileInfo}')
-
-        fileNameNoExt = os.path.basename(fileInfo).split('.nc')[0]
-
-        # NetCDF 파일 읽기
-        data = xr.open_dataset(fileInfo)
-
-        # data['scfg'].values
-        # 0=snow free, 1-100=SCF[%], 205=Cloud, 206=Night, 210=Water, 215=Glaciers, icecaps, ice sheets, 252|253|254=ERROR, 255=Not valid data
-        # data['scfg_unc'].values
-        # data['spatial_ref'].values
-
-        lonList = np.arange(sysOpt['grid']['lonMin'], sysOpt['grid']['lonMax'], sysOpt['grid']['lonInv'])
-        latList = np.arange(sysOpt['grid']['latMin'], sysOpt['grid']['latMax'], sysOpt['grid']['latInv'])
-        dataL1 = data.isel(nv=0, time=0).sel(lon=lonList, lat=latList, method='nearest')
-
-        # dataL1['scfg'].plot()
-        # plt.show()
-
-        # flag 정보 없음
-        # scfg 기준으로 252 이하 설정
-        dataL2 = dataL1['scfg'].where((dataL1['scfg'] < 252))
-
-        lat1D = dataL2['lat'].values
-        lon1D = dataL2['lon'].values
-
-        selData = xr.Dataset(
-            {
-                'scfg': (('time', 'lat', 'lon'), (dataL2.values).reshape(1, len(lat1D), len(lon1D)))
-            }
-            , coords={
-                'time': pd.date_range(dtDayInfo, periods=1)
-                , 'lat': lat1D
-                , 'lon': lon1D
-            }
-        )
-
-        # ***********************************************************
-        # 2. 0.25x0.25도 해상도로 재격자화 한 후 북반구 영역만 nc 파일로 생성
-        # ***********************************************************
-        # saveNcFile = '{}/{}-{}.png'.format('/home/sbpark/analysis/python_resources/4satellites/20230723/figs', satType, obsDateTime)
-        saveNcFile = '{}/{}-{}.nc'.format('/DATA/OUTPUT/LSH0442', fileNameNoExt, 'prop')
-        os.makedirs(os.path.dirname(saveNcFile), exist_ok=True)
-        selData.to_netcdf(saveNcFile)
-        print(f'[CHECK] saveNcFile : {saveNcFile}')
-
-        # ***********************************************************
-        # 시각화
-        # ***********************************************************
-        dataL3 = dataL2.to_dataframe().reset_index(drop=False).dropna()
-        lon1D = dataL3['lon'].values
-        lat1D = dataL3['lat'].values
-        val1D = dataL3['scfg'].values
-
-        #   # 0=snow free, 1-100=SCF[%], 205=Cloud, 206=Night, 210=Water, 215=Glaciers, icecaps, ice sheets, 252|253|254=ERROR, 255=Not valid data
-        cateList = {0: 'snow free', 1: 'SCF %', 205: 'Cloud', 206: 'Night', 210: 'Water', 215: 'Glaciers', 251: 'Null'}
-        colors = ['blue', 'gray', 'orange', 'yellow', 'green', 'cyan', 'black']
-        bounds = list(cateList.keys())
-
-        # Create a custom colormap and norm based on your categories
-        cmap = mcolors.ListedColormap(colors)
-        norm = mcolors.BoundaryNorm(bounds, cmap.N)
-
-        # Create a scatter plot
-        fig, ax = plt.subplots()
-        sc = ax.scatter(lon1D, lat1D, c=val1D, cmap=cmap, norm=norm, s=1.0)
-
-        # Create a colorbar with the custom colormap
-        cb = plt.colorbar(sc, ticks=bounds)
-        cb.set_ticklabels(list(cateList.values()))
-
-        # saveImg = '{}/{}-{}.png'.format('/home/sbpark/analysis/python_resources/4satellites/20230723/figs', satType, obsDateTime)
-        saveImg = '{}/{}-{}.png'.format('/DATA/FIG/LSH0442', fileNameNoExt, 'prop')
-        os.makedirs(os.path.dirname(saveImg), exist_ok=True)
-
-        cb.set_label(None)
-        plt.xlabel(None)
-        plt.ylabel(None)
-        plt.grid(True)
-        plt.savefig(saveImg, dpi=600, bbox_inches='tight', transparent=True)
-        plt.tight_layout()
-        plt.show()
-        plt.close()
-
-        print(f'[CHECK] saveImg : {saveImg}')
-
-# inpFile = '{}/{}'.format('/DATA/OUTPUT/LSH0442', '*-ESACCI-L3C_SNOW-SCFG-MODIS_TERRA-fv2.0-prop.nc')
-# fileList = sorted(glob.glob(inpFile))
+# dataL3 = xr.Dataset()
+# for dtDayIdx, dtDayInfo in enumerate(dtDayList):
+#     # print(f'[CHECK] dtDayInfo : {dtDayInfo}')
 #
-# data = xr.open_mfdataset(fileList)
+#     # inpFile = '{}/{}'.format('/home/data/satellite/OCO3/OCO3_L2_Lite_SIF.10r', 'oco3_LtSIF_%y%m%d_B*_*.nc4')
+#     inpFile = '{}/{}'.format( '/DATA/INPUT/LSH0442', '%Y%m%d-ESACCI-L3C_SNOW-SCFG-MODIS_TERRA-fv2.0.nc')
+#     inpFileDate = dtDayInfo.strftime(inpFile)
+#     fileList = sorted(glob.glob(inpFileDate))
 #
-# scfg = data['scfg']
-# # 0=snow free, 1-100=SCF[%], 205=Cloud, 206=Night, 210=Water, 215=Glaciers, icecaps, ice sheets, 252|253|254=ERROR, 255=Not valid data
+#     if fileList is None or len(fileList) < 1: continue
 #
-# scfgL1 = scfg.where((scfg <= 100))
+#     for fileInfo in fileList:
 #
-# # 10일 이동평균 계산
-# movMean = scfgL1.rolling(time=10, center=True).mean()
+#         print(f'[CHECK] fileInfo : {fileInfo}')
 #
-# # 10일 이동평균이 0이고, 원래의 눈 덮인 지역이 0인 마지막 날 찾기
+#         fileNameNoExt = os.path.basename(fileInfo).split('.nc')[0]
+#
+#         # NetCDF 파일 읽기
+#         data = xr.open_dataset(fileInfo)
+#
+#         # data['scfg'].values
+#         # 0=snow free, 1-100=SCF[%], 205=Cloud, 206=Night, 210=Water, 215=Glaciers, icecaps, ice sheets, 252|253|254=ERROR, 255=Not valid data
+#         # data['scfg_unc'].values
+#         # data['spatial_ref'].values
+#
+#         lonList = np.arange(sysOpt['grid']['lonMin'], sysOpt['grid']['lonMax'], sysOpt['grid']['lonInv'])
+#         latList = np.arange(sysOpt['grid']['latMin'], sysOpt['grid']['latMax'], sysOpt['grid']['latInv'])
+#         dataL1 = data.isel(nv=0, time=0).sel(lon=lonList, lat=latList, method='nearest')
+#
+#         # dataL1['scfg'].plot()
+#         # plt.show()
+#
+#         # flag 정보 없음
+#         # scfg 기준으로 252 이하 설정
+#         dataL2 = dataL1['scfg'].where((dataL1['scfg'] < 252))
+#
+#         lat1D = dataL2['lat'].values
+#         lon1D = dataL2['lon'].values
+#
+#         selData = xr.Dataset(
+#             {
+#                 'scfg': (('time', 'lat', 'lon'), (dataL2.values).reshape(1, len(lat1D), len(lon1D)))
+#             }
+#             , coords={
+#                 'time': pd.date_range(dtDayInfo, periods=1)
+#                 , 'lat': lat1D
+#                 , 'lon': lon1D
+#             }
+#         )
+#
+#         # ***********************************************************
+#         # 2. 0.25x0.25도 해상도로 재격자화 한 후 북반구 영역만 nc 파일로 생성
+#         # ***********************************************************
+#         # saveNcFile = '{}/{}-{}.png'.format('/home/sbpark/analysis/python_resources/4satellites/20230723/figs', satType, obsDateTime)
+#         saveNcFile = '{}/{}-{}.nc'.format('/DATA/OUTPUT/LSH0442', fileNameNoExt, 'prop')
+#         os.makedirs(os.path.dirname(saveNcFile), exist_ok=True)
+#         selData.to_netcdf(saveNcFile)
+#         print(f'[CHECK] saveNcFile : {saveNcFile}')
+#
+#         # ***********************************************************
+#         # 시각화
+#         # ***********************************************************
+#         dataL3 = dataL2.to_dataframe().reset_index(drop=False).dropna()
+#         lon1D = dataL3['lon'].values
+#         lat1D = dataL3['lat'].values
+#         val1D = dataL3['scfg'].values
+#
+#         #   # 0=snow free, 1-100=SCF[%], 205=Cloud, 206=Night, 210=Water, 215=Glaciers, icecaps, ice sheets, 252|253|254=ERROR, 255=Not valid data
+#         cateList = {0: 'snow free', 1: 'SCF %', 205: 'Cloud', 206: 'Night', 210: 'Water', 215: 'Glaciers', 251: 'Null'}
+#         colors = ['blue', 'gray', 'orange', 'yellow', 'green', 'cyan', 'black']
+#         bounds = list(cateList.keys())
+#
+#         # Create a custom colormap and norm based on your categories
+#         cmap = mcolors.ListedColormap(colors)
+#         norm = mcolors.BoundaryNorm(bounds, cmap.N)
+#
+#         # Create a scatter plot
+#         fig, ax = plt.subplots()
+#         sc = ax.scatter(lon1D, lat1D, c=val1D, cmap=cmap, norm=norm, s=1.0)
+#
+#         # Create a colorbar with the custom colormap
+#         cb = plt.colorbar(sc, ticks=bounds)
+#         cb.set_ticklabels(list(cateList.values()))
+#
+#         # saveImg = '{}/{}-{}.png'.format('/home/sbpark/analysis/python_resources/4satellites/20230723/figs', satType, obsDateTime)
+#         saveImg = '{}/{}-{}.png'.format('/DATA/FIG/LSH0442', fileNameNoExt, 'prop')
+#         os.makedirs(os.path.dirname(saveImg), exist_ok=True)
+#
+#         cb.set_label(None)
+#         plt.xlabel(None)
+#         plt.ylabel(None)
+#         plt.grid(True)
+#         plt.savefig(saveImg, dpi=600, bbox_inches='tight', transparent=True)
+#         plt.tight_layout()
+#         plt.show()
+#         plt.close()
+#
+#         print(f'[CHECK] saveImg : {saveImg}')
+
+inpFile = '{}/{}'.format('/DATA/OUTPUT/LSH0442', '*-ESACCI-L3C_SNOW-SCFG-MODIS_TERRA-fv2.0-prop.nc')
+fileList = sorted(glob.glob(inpFile))
+
+data = xr.open_mfdataset(fileList)
+
+scfg = data['scfg']
+# 0=snow free, 1-100=SCF[%], 205=Cloud, 206=Night, 210=Water, 215=Glaciers, icecaps, ice sheets, 252|253|254=ERROR, 255=Not valid data
+
+scfgL1 = scfg.where((scfg <= 100))
+
+# 10일 이동평균 계산
+movMean = scfgL1.rolling(time=10, center=True).sum()
+
+# movMean.isel(time = 11).plot()
+# plt.show()
+
+# 각 격자점에서 end of snowmelt를 찾기 위한 함수
+def find_end_of_snowmelt(da):
+    # 연속적인 0 값을 찾기 위한 Boolean 마스크 생성
+    zeros_mask = (da == 0)
+
+    # 연속적인 0 값을 갖는 시간의 길이를 계산
+    lengths = zeros_mask.groupby('time.year').apply(lambda y: y.cumsum(dim='time') - y.cumsum(dim='time').where(~y).ffill(dim='time').fillna(0))
+
+    # 각 해마다 마지막 연속 0값 찾기
+    end_of_snowmelt = lengths.groupby('time.year').apply(lambda y: y.where(y.groupby('time.year').max() == y).dropna(dim='time', how='all').time)
+
+    # day of year로 변환
+    return end_of_snowmelt.dt.dayofyear
+
+
+grouped = movMean.stack(points=('lat', 'lon'))
+end_of_snowmelt = grouped.groupby('points').apply(find_end_of_snowmelt)
+end_of_snowmelt2 = end_of_snowmelt.unstack('points')
+
+# 각 격자점에서 end of snowmelt 계산
+# test = movMean.groupby(('time')).apply(find_end_of_snowmelt)
+# end_of_snowmelt = grouped.groupby('lat').apply(find_end_of_snowmelt)
+# end_of_snowmelt = grouped.apply(lambda x: x.groupby('lon').apply(find_end_of_snowmelt))
+# end_of_snowmelt = movMean.groupby('time').groupby('lat').apply(find_end_of_snowmelt)
+# end_of_snowmelt = movMean.groupby(['time']).apply(find_end_of_snowmelt)
+
+print('asdasdasd')
+
+# 10일 이동평균이 0이고, 원래의 눈 덮인 지역이 0인 마지막 날 찾기
 # condition = (movMean == 0)
 # last_true = condition.where(condition).resample(time='AS').map(lambda x: x.idxmax(dim='time'))
-#
+
 # last_true.isel(time=0)
 # last_true.isel(time=0).plot()
 # plt.show()
-#
-# doy_last_true = last_true.dt.dayofyear
+
 
 
