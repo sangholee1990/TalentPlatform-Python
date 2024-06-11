a312eec600d54de28654c474c084b746006f95b0
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2024-06-12 06:27:54 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/talentPlatform/unitSys/TalentPlatform-LSH0454-MergeData-monthPopTrend.py
@@ -0,0 +1,350 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import glob
+import logging
+import logging.handlers
+import os
+import platform
+import sys
+import traceback
+from datetime import datetime
+
+import matplotlib as mpl
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+import pandas as pd
+
+# =================================================
+# 사용자 매뉴얼
+# =================================================
+# [소스 코드의 실행 순서]
+# 1. 초기 설정 : 폰트 설정
+# 2. 유틸리티 함수 : 초기화 함수 (로그 설정, 초기 변수, 초기 전달인자 설정) 또는 자주 사용하는 함수
+# 3. 주 프로그램 :부 프로그램을 호출
+# 4. 부 프로그램 : 자료 처리를 위한 클래스로서 내부 함수 (초기 변수, 비즈니스 로직, 수행 프로그램 설정)
+# 4.1. 환경 변수 설정 (로그 설정) : 로그 기록을 위한 설정 정보 읽기
+# 4.2. 환경 변수 설정 (초기 변수) : 입력 경로 (inpPath) 및 출력 경로 (outPath) 등을 설정
+# 4.3. 초기 변수 (Argument, Option) 설정 : 파이썬 실행 시 전달인자 설정 (pyhton3 *.py argv1 argv2 argv3 ...)
+# 4.4. 비즈니스 로직 수행 : 단위 시스템 (unit 파일명)으로 관리 또는 비즈니스 로직 구현
+
+# =================================================
+# 1. 초기 설정
+# =================================================
+# warnings.filterwarnings('ignore')
+
+# plt.rc('font', family='Malgun Gothic')
+plt.rc('axes', unicode_minus=False)
+# sns.set(font='Malgun Gothic', rc={'axes.unicode_minus': False}, style='darkgrid')
+
+# 그래프에서 마이너스 글꼴 깨지는 문제에 대한 대처
+mpl.rcParams['axes.unicode_minus'] = False
+
+
+# =================================================
+# 2. 유틸리티 함수
+# =================================================
+# 로그 설정
+def initLog(env=None, contextPath=None, prjName=None):
+    if env is None: env = 'local'
+    if contextPath is None: contextPath = os.getcwd()
+    if prjName is None: prjName = 'test'
+
+    saveLogFile = "{}/{}_{}_{}_{}_{}_{}.log".format(
+        contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'log', prjName)
+        , platform.system()
+        , platform.machine()
+        , platform.architecture()[0]
+        , platform.node()
+        , prjName
+        , datetime.now().strftime("%Y%m%d")
+    )
+
+    os.makedirs(os.path.dirname(saveLogFile), exist_ok=True)
+
+    # logger instance 생성
+    log = logging.getLogger(prjName)
+
+    if len(log.handlers) > 0: return log
+
+    # format 생성
+    format = logging.Formatter('%(asctime)s [%(name)s | %(lineno)d | %(filename)s] [%(levelname)-5.5s] %(message)s')
+
+    # handler 생성
+    streamHandler = logging.StreamHandler()
+    fileHandler = logging.FileHandler(saveLogFile)
+
+    # logger instance에 format 설정
+    streamHandler.setFormatter(format)
+    fileHandler.setFormatter(format)
+
+    # logger instance에 handler 설정
+    log.addHandler(streamHandler)
+    log.addHandler(fileHandler)
+
+    # logger instance로 log 기록
+    log.setLevel(level=logging.INFO)
+
+    return log
+
+
+#  초기 변수 설정
+def initGlobalVar(env=None, contextPath=None, prjName=None):
+    if env is None: env = 'local'
+    if contextPath is None: contextPath = os.getcwd()
+    if prjName is None: prjName = 'test'
+
+    # 환경 변수 (local, 그 외)에 따라 전역 변수 (입력 자료, 출력 자료 등)를 동적으로 설정
+    # 즉 local의 경우 현재 작업 경로 (contextPath)를 기준으로 설정
+    # 그 외의 경우 contextPath/resources/input/prjName와 같은 동적으로 구성
+    globalVar = {
+        'prjName': prjName
+        , 'sysOs': platform.system()
+        , 'contextPath': contextPath
+        , 'resPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources')
+        , 'cfgPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config')
+        , 'inpPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'input', prjName)
+        , 'figPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'fig', prjName)
+        , 'outPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'input', prjName)
+        , 'movPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'movie', prjName)
+        , 'logPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'log', prjName)
+        , 'mapPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config', 'mapInfo')
+        , 'sysPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config', 'system.cfg')
+        , 'seleniumPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config', 'selenium')
+        , 'fontPath': contextPath if env in 'local' else os.path.join(contextPath, 'resources', 'config', 'fontInfo')
+    }
+
+    return globalVar
+
+
+#  초기 전달인자 설정
+def initArgument(globalVar, inParams):
+
+    # 원도우 또는 맥 환경
+    if globalVar['sysOs'] in 'Windows' or globalVar['sysOs'] in 'Darwin':
+        inParInfo = inParams
+
+    # 리눅스 환경
+    if globalVar['sysOs'] in 'Linux':
+        parser = argparse.ArgumentParser()
+
+        for i, argv in enumerate(sys.argv[1:]):
+            if not argv.__contains__('--'): continue
+            parser.add_argument(argv)
+
+        inParInfo = vars(parser.parse_args())
+
+        # 글꼴 설정
+        fontList = glob.glob('{}/{}'.format(globalVar['fontPath'], '*.ttf'))
+        if (len(fontList) > 0):
+            fontName = font_manager.FontProperties(fname=fontList[0]).get_name()
+            plt.rcParams['font.family'] = fontName
+            plt.rc('font', family=fontName)
+
+    log.info('[CHECK] inParInfo : {}'.format(inParInfo))
+
+    for key, val in inParInfo.items():
+        if val is None: continue
+        # 전역 변수에 할당
+        globalVar[key] = val
+
+    # 전역 변수
+    for key, val in globalVar.items():
+        if env not in 'local' and key.__contains__('Path') and env and not os.path.exists(val):
+            os.makedirs(val)
+
+        globalVar[key] = val.replace('\\', '/')
+
+        log.info("[CHECK] {} : {}".format(key, val))
+
+        # self 변수에 할당
+        # setattr(self, key, val)
+
+    return globalVar
+
+def getFloorArea(size):
+  if size >= 114:
+    return "43평"
+  elif size >= 80:
+    return "32평"
+  elif size >= 60:
+    return "24평"
+  elif size >= 40:
+    return "18평"
+  elif size >= 20:
+    return "9평"
+  elif size >= 10:
+    return "5평"
+  else:
+    return "5평 미만"
+
+def getAmountType(amount):
+    if amount > 15:
+        return "15억 초과"
+    elif amount >= 9:
+        return "9-15억"
+    elif amount >= 6:
+        return "6-9억"
+    elif amount > 3:
+        return "3-6억"
+    else:
+        return "3억 이하"
+
+# ================================================
+# 4. 부 프로그램
+# ================================================
+class DtaProcess(object):
+
+    # ================================================
+    # 요구사항
+    # ================================================
+    # Python을 이용한 부동산 데이터 분석 및 가격 예측 고도화 및 구글 스튜디오 시각화
+
+    # G:\내 드라이브\shlee\04. TalentPlatform\[재능플랫폼] 최종납품\[요청] LSH0454. Python을 이용한 부동산 데이터 분석 및 가격 예측 고도화 및 구글 스튜디오 시각화\20240310_빅쿼리 연계
+
+    # ================================================================================================
+    # 환경변수 설정
+    # ================================================================================================
+    global env, contextPath, prjName, serviceName, log, globalVar
+
+    # env = 'local'  # 로컬 : 원도우 환경, 작업환경 (현재 소스 코드 환경 시 .) 설정
+    env = 'dev'  # 개발 : 원도우 환경, 작업환경 (사용자 환경 시 contextPath) 설정
+    # env = 'oper'  # 운영 : 리눅스 환경, 작업환경 (사용자 환경 시 contextPath) 설정
+
+    if (platform.system() == 'Windows'):
+        contextPath = os.getcwd() if env in 'local' else 'E:/04. TalentPlatform/Github/TalentPlatform-Python'
+    else:
+        contextPath = os.getcwd() if env in 'local' else '/SYSTEMS/PROG/PYTHON/PyCharm'
+
+    prjName = 'test'
+    serviceName = 'LSH0454'
+
+    # 4.1. 환경 변수 설정 (로그 설정)
+    log = initLog(env, contextPath, prjName)
+
+    # 4.2. 환경 변수 설정 (초기 변수)
+    globalVar = initGlobalVar(env, contextPath, prjName)
+
+    # ================================================================================================
+    # 4.3. 초기 변수 (Argument, Option) 설정
+    # ================================================================================================
+    def __init__(self, inParams):
+
+        log.info('[START] __init__ : {}'.format('init'))
+
+        try:
+            # 초기 전달인자 설정 (파이썬 실행 시)
+            # pyhton3 *.py argv1 argv2 argv3 ...
+            initArgument(globalVar, inParams)
+
+        except Exception as e:
+            log.error('Exception : {}'.format(e))
+            raise e
+        finally:
+            log.info('[END] __init__ : {}'.format('init'))
+
+    # ================================================================================================
+    # 4.4. 비즈니스 로직 수행
+    # ================================================================================================
+    def exec(self):
+        log.info('[START] {}'.format('exec'))
+
+        try:
+            if platform.system() == 'Windows':
+                pass
+            else:
+                globalVar['inpPath'] = '/DATA/INPUT'
+                globalVar['outPath'] = '/DATA/OUTPUT'
+                globalVar['figPath'] = '/DATA/FIG'
+                globalVar['updPath'] = '/DATA/CSV'
+
+            sysOpt = {
+                'sheetName': '실거래가 전처리'
+                , 'metaData': {
+                    "충남": "충청남도",
+                    "충북": "충청북도",
+                    "서울": "서울특별시",
+                    "경기": "경기도",
+                    "부산": "부산광역시",
+                    "대구": "대구광역시",
+                    "인천": "인천광역시",
+                    "광주": "광주광역시",
+                    "대전": "대전광역시",
+                    "울산": "울산광역시",
+                    "세종": "세종특별자치시",
+                    "강원": "강원도",
+                    "전북": "전라북도",
+                    "전남": "전라남도",
+                    "경북": "경상북도",
+                    "경남": "경상남도",
+                    "제주": "제주특별자치도"
+                }
+            }
+
+            # *********************************************************************************
+            # 코드 정보 읽기
+            # *********************************************************************************
+            colNameList = ['세대수', '단지명', 'sgg']
+            colCodeList = ['numHouse', 'comName', 'sgg']
+
+            renameDict = {colName: colCode for colName, colCode in zip(colNameList, colCodeList)}
+
+            # *********************************************************************************
+            # 파일 읽기
+            # *********************************************************************************
+            inpFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, '대단지 순위_전체_*.xlsx')
+            fileList = sorted(glob.glob(inpFile), reverse=True)
+            if fileList is None or len(fileList) < 1:
+                log.error(f'[ERROR] inpFile : {inpFile} / 입력 자료를 확인해주세요.')
+
+            dataL2 = pd.DataFrame()
+            for fileInfo in fileList:
+                log.info(f'[CHECK] fileInfo : {fileInfo}')
+
+                fileName = os.path.basename(fileInfo)
+                fileNameNoExt = fileName.split('.')[0]
+
+                data = pd.read_excel(fileInfo, skiprows=1, engine='openpyxl')
+                data['sgg'] = data['시도'] + " " + data['시군구']
+
+                dataL1 = data[colNameList].rename(columns=renameDict, inplace=False)
+                dataL2 = pd.concat([dataL2, dataL1], axis=0)
+
+            # CSV 생성
+            saveFile = '{}/{}/{}_{}.csv'.format(globalVar['outPath'], serviceName, datetime.now().strftime("%Y%m%d"), 'TB_LARGE-COM-RANK')
+            os.makedirs(os.path.dirname(saveFile), exist_ok=True)
+            dataL2.to_csv(saveFile, index=False)
+            log.info(f'[CHECK] saveFile : {saveFile}')
+
+        except Exception as e:
+            log.error('Exception : {}'.format(e))
+            raise e
+        finally:
+            log.info('[END] {}'.format('exec'))
+
+# ================================================
+# 3. 주 프로그램
+# ================================================
+if __name__ == '__main__':
+
+    print('[START] {}'.format('main'))
+
+    try:
+
+        # 파이썬 실행 시 전달인자를 초기 환경변수 설정
+        inParams = {}
+
+        print('[CHECK] inParams : {}'.format(inParams))
+
+        # 부 프로그램 호출
+        subDtaProcess = DtaProcess(inParams)
+
+        subDtaProcess.exec()
+
+    except Exception as e:
+        print(traceback.format_exc())
+        sys.exit(1)
+
+    finally:
+        print('[END] {}'.format('main'))
