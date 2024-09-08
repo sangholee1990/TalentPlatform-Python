14277a56232c1642ecf51666e9609c027bf1fcc1
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2024-09-09 05:36:23 +0900

    Merge remote-tracking branch 'origin/master'

--- src/talentPlatform/unitSys/TalentPlatform-LSH0575-DaemonFramework.py
+++ src/talentPlatform/unitSys/TalentPlatform-LSH0575-DaemonFramework.py
@@ -20,6 +20,7 @@ from matplotlib import font_manager, rc
 import seaborn as sns
 
 from scipy.interpolate import Rbf
+from xarray.util.generate_aggregations import skipna
 
 # =================================================
 # 사용자 매뉴얼
@@ -267,48 +268,10 @@ class DtaProcess(object):
             log.info(f'[CHECK] len(lonList) : {len(lonList)}')
             log.info(f'[CHECK] len(latList) : {len(latList)}')
 
-            inpFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, '*/gpw_v4_national_identifier_grid_rev11_30_sec.shp')
-            fileList = sorted(glob.glob(inpFile))
-            fileInfo = fileList[0]
-            shpData = gpd.read_file(fileInfo)
 
-            # [CHECK] shpCode : KOR / shpVal : 410 / shpName : Republic of Korea / shpGeoCen : POINT (127.75250070905732 36.30623116907186)
-            for idx, item in shpData.iterrows():
-                shpCode = item.ISOCODE
-                shpName = item.NAME0
-                shpVal = item.Value
-                shpGeoCen = item.geometry.centroid
-                log.info(f'[CHECK] shpCode : {shpCode} / shpVal : {shpVal} / shpName : {shpName} / shpGeoCen : {shpGeoCen}')
-
-            # shpDataL1 = shpData.iloc[0, ]
-            # Value                                                         4
-            # ISOCODE                                                     AFG
-            # UNSDCODE                                                      4
-            # NAME0                                               Afghanistan
-            # CIESINCODE                                                    4
-            # DATATYPE                         Population estimate/projection
-            # DATACODE                                                      6
-            # DATAYEAR                                                   2011
-            # DATALEVEL                                                     2
-            # SEXLEVEL                                                      2
-            # AGELEVEL                                                      0
-            # GRSTART                                                    1998
-            # GREND                                                      2011
-            # GRLEVEL                                                       1
-            # LASTCENSUS                                                 1979
-            # MEANUNITKM                                          4507.466751
-            # geometry      POLYGON ((60.90833333333333 29.833333333333343...
-
-
-
-
-            # shpData.head()
-            # shpData.info()
-            # shpData.describe()
-
-            # shpData.plot()
-            # plt.show()
-
+            # =========================================================
+            # 엑셀 파일 읽기
+            # =========================================================
             xlsxFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, 'WPP2024_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT.xlsx')
             xlsxList = sorted(glob.glob(xlsxFile))
             xlsxInfo = xlsxList[0]
@@ -316,32 +279,193 @@ class DtaProcess(object):
 
             # xlsxData.columns
             xlsxColList = ['ISO3 Alpha-code', 'Location code', 'Year', 'Total Population, as of 1 January (thousands)']
-            xlsxDataL1 = xlsxData.loc[
-                (xlsxData['ISO3 Alpha-code'] == 'KOR')
-                & (xlsxData['Location code'] == 410)][xlsxColList]
 
-            # xlsxDataL1
+            locCodeList = np.unique(xlsxData['Location code'])
+            xlsxDataL5 = pd.DataFrame()
+            for locCode in locCodeList:
+                xlsxDataL1 = xlsxData.loc[
+                    (xlsxData['Location code'] == locCode)
+                    & (2019 <= xlsxData['Year']) & (xlsxData['Year'] <= 2025)
+                    ]
+                if len(xlsxDataL1) < 1: continue
 
-            xlsxDataL2 = xlsxDataL1.copy().rename(columns={'Total Population, as of 1 January (thousands)': 'orgVal'})
-            xlsxDataL2['dtDate'] = pd.to_datetime(xlsxDataL2['Year'], format='%Y')
-            # xlsxDataL2.set_index('dtDate', inplace=True)
-            # xlsxDataL3['newVal'] = xlsxDataL3['orgVal'].interpolate(method='linear')
+                log.info(f'[CHECK] locCode : {locCode}')
 
-            xlsxDataL2['dtXran'] = pd.to_numeric(xlsxDataL2['dtDate'].apply(lambda x: convDateToDeci(x)), errors='coerce')
-            xlsxDataL2['orgVal'] = pd.to_numeric(xlsxDataL2['orgVal'], errors='coerce')
+                xlsxDataL2 = xlsxDataL1.copy().rename(columns={'Total Population, as of 1 January (thousands)': 'orgVal'})
+                xlsxDataL2['dtDate'] = pd.to_datetime(xlsxDataL2['Year'], format='%Y')
+                # xlsxDataL2.set_index('dtDate', inplace=True)
+                # xlsxDataL3['newVal'] = xlsxDataL3['orgVal'].interpolate(method='linear')
 
-            xlsxDataL3 = xlsxDataL2.copy().set_index('dtDate').resample('1D').asfreq()
-            xlsxDataL3['dtXran'] = pd.to_numeric(xlsxDataL3.index.to_series().apply(lambda x: convDateToDeci(x)), errors='coerce')
+                xlsxDataL2['dtXran'] = pd.to_numeric(xlsxDataL2['dtDate'].apply(lambda x: convDateToDeci(x)), errors='coerce')
+                xlsxDataL2['orgVal'] = pd.to_numeric(xlsxDataL2['orgVal'], errors='coerce')
 
-            # Radial basis function (RBF) interpolation in N dimensions.
-            try:
-                rbfModel = Rbf(xlsxDataL2['dtXran'].values, xlsxDataL2['orgVal'].values, function='linear')
-                rbfRes = rbfModel(xlsxDataL3['dtXran'].values)
-                xlsxDataL3['newVal'] = rbfRes
-            except Exception as e:
-                log.error(f"Exception : {e}")
+                xlsxDataL3 = xlsxDataL2.copy().set_index('dtDate').resample('1D').asfreq()
+                xlsxDataL3['dtXran'] = pd.to_numeric(xlsxDataL3.index.to_series().apply(lambda x: convDateToDeci(x)), errors='coerce')
 
-            xlsxDataL4 = xlsxDataL3.reset_index()
+                # Radial basis function (RBF) interpolation in N dimensions.
+                try:
+                    rbfModel = Rbf(xlsxDataL2['dtXran'].values, xlsxDataL2['orgVal'].values, function='linear')
+                    rbfRes = rbfModel(xlsxDataL3['dtXran'].values)
+                    xlsxDataL3['newVal'] = rbfRes
+                except Exception as e:
+                    log.error(f"Exception : {e}")
+
+                xlsxDataL4 = xlsxDataL3.reset_index()[['dtDate', 'orgVal', 'newVal']]
+                xlsxDataL4['code'] = xlsxDataL2['ISO3 Alpha-code'].values[0]
+                xlsxDataL4['locCode'] = xlsxDataL2['Location code'].values[0]
+
+                xlsxDataL5 = pd.concat([xlsxDataL5, xlsxDataL4], axis=0)
+
+            # =========================================================
+            # gpw 데이터 읽기/병합
+            # =========================================================
+            inpFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, '*/gpw_v4_national_identifier_grid_rev11_30_sec.tif')
+            fileList = sorted(glob.glob(inpFile))
+            # if (len(fileList) < 1): continue
+            fileInfo = fileList[0]
+            gpwNatData = xr.open_rasterio(fileInfo)
+            gpwNatDataL1 = gpwNatData.sel(band=1)
+
+            inpFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, '*/gpw_v4_population_count_rev11_2000_30_sec.tif')
+            fileList = sorted(glob.glob(inpFile))
+            # if (len(fileList) < 1): continue
+            fileInfo = fileList[0]
+            gpwPopData = xr.open_rasterio(fileInfo)
+            gpwPopDataL1 = gpwPopData.sel(band=1).to_dataset(name='POP')
+
+            # 데이터 병합
+            gpwData = gpwPopDataL1.copy()
+            gpwData.coords['NAT'] = (('y', 'x'), gpwNatDataL1.values)
+
+            # gpwDataL1 = gpwData
+            gpwDataL1 = gpwData.sel(x=slice(130, 131)).isel(y=slice(5500, 6000))
+
+            gpwDataL1['NAT'] = xr.where((gpwDataL1['NAT'] == gpwNatData.nodatavals), np.nan, gpwDataL1['NAT'])
+            gpwDataL1['POP'] = xr.where((gpwDataL1['POP'] == gpwPopData.nodatavals), np.nan, gpwDataL1['POP'])
+
+            # NAT에 따른 합계
+            sumData = gpwDataL1.groupby(gpwDataL1['NAT']).sum()
+
+
+            # natList = np.unique(gpwDataL1['NAT'].values)
+            # for nat in natList:
+            #     if pd.isna(nat): continue
+            #     log.info(f'[CHECK] nat : {nat}')
+            #
+            #     gpwDataL3 = gpwDataL2.sel(NAT = nat)
+            #
+            #     sumDataL1 = sumData.sel(NAT = nat)
+            #     sumVal = sumDataL1['POP'].values
+
+            gpwDataL2 = gpwDataL1.copy()
+            locCodeList = np.unique(xlsxDataL5['locCode'])
+            for locCode in locCodeList:
+                log.info(f'[CHECK] locCode : {locCode}')
+
+                gpwDataL3 = gpwDataL2.where(gpwDataL2['NAT'] == locCode, drop=True)
+
+
+                sumDataL1 = sumData.sel(NAT = locCode)
+                sumVal = sumDataL1['POP'].values
+
+            # gpwNatDataL1['x'].values
+
+            # gpwNatDataL1['NAT'].plot()
+            # plt.show()
+
+            # gpwPopDataL1 = gpwPopData.to_dataset(name='POP')
+            # gpwPopDataL1 = gpwPopData.to_dataset(name='POP').isel(x=slice(120, 150), y=slice(40, 50))
+            # gpwPopDataL1 = gpwPopData.sel(band = 1).to_dataset(name='POP').sel(x=slice(130, 131)).isel(y=slice(5500, 6000))
+
+            # gpwNatDataL1['NAT'].plot()
+            # gpwPopDataL1['POP'].plot()
+            # plt.show()
+
+
+            # gpwData = xr.merge([gpwNatDataL1, gpwPopDataL1])
+
+            # gpwData
+
+            # isMask = gpwNatDataL1['NAT'] == 156
+            # gpwPopDataL1['POP'] * isMask.values
+            #
+            # gpwPopDataL1.coords['NAT'] = (('y', 'x'), gpwNatDataL1['NAT'].values)
+            #
+            # isMask = gpwPopDataL1['NAT'] == 156
+            # gpwPopDataL1['POP'] * isMask
+            #
+            # # isMask = (gpwData['NAT'] == 156)
+            #
+            # gg = gpwPopDataL1['POP'].groupby(gpwPopDataL1['NAT']).sum()
+            # gg.sel(NAT = 156).values
+
+
+
+            # gpwData['NAT'].where(isMask, drop=True).values
+
+            #
+            # gpwData['NAT'] = xr.where((gpwData['NAT'] == gpwNatData.nodatavals), np.nan, gpwData['NAT'])
+            # gpwData['POP'] = xr.where((gpwData['POP'] == gpwPopData.nodatavals), np.nan, gpwData['POP'])
+            #
+            # # gpwDataL1 = gpwData.sel(band=1)
+            # gpwDataL1 = gpwData
+            #
+            # # np.unique(gpwDataL1['NAT'].values)
+            # # np.unique(gpwDataL1['POP'].values)
+            #
+            # # array([156., 408., 643.,  nan])
+            # # isMask = (gpwNatData.sel(band=1) == 156)
+            # isMask = (gpwDataL1['NAT'] == 156)
+            # # gpwNatData[isFlag]
+
+
+
+
+            # gpwNatData.where(isMask)
+            # gpwDataL1.where(isMask)
+
+            # b = gpwDataL1['NAT'].where(isMask, drop=False)
+            #
+            # # pop_sum = gpwDataL1.groupby(gpwDataL1['NAT']).sum()
+            #
+            # # gpwDataL1.groupby(gpwDataL1['NAT']).sum( skipna=True)
+            #
+            # # gpwDataL1.sel(NAT = 156)
+            #
+            # isMask = (gpwDataL1['NAT'] == 156)
+            # gpwDataL1['NAT'].where(isMask, drop=True).values
+            #
+            # # gpwData['x'].values
+            #
+            # # gpwDataL2 = gpwDataL1.to_dataframe().reset_index(drop=False)
+            # gpwDataL2 = gpwData.to_dataframe().reset_index(drop=False)
+            # gpwDataL2.dropna().groupby(['NAT']).sum().reset_index()
+            #
+
+
+            # =========================================================
+            # gpw 데이터 읽기/병합
+            # =========================================================
+            # inpFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, '*/gpw_v4_national_identifier_grid_rev11_30_sec.shp')
+            # fileList = sorted(glob.glob(inpFile))
+            # fileInfo = fileList[0]
+            # shpData = gpd.read_file(fileInfo)
+            #
+            # # shpData.head()
+            # # shpData.info()
+            # # shpData.describe()
+            #
+            # # shpData.plot()
+            # # plt.show()
+            #
+            # # [CHECK] shpCode : KOR / shpVal : 410 / shpName : Republic of Korea / shpGeoCen : POINT (127.75250070905732 36.30623116907186)
+            # for idx, item in shpData.iterrows():
+            #     shpCode = item.ISOCODE
+            #     shpName = item.NAME0
+            #     shpVal = item.Value
+            #     shpGeoCen = item.geometry.centroid
+            #     log.info(f'[CHECK] shpCode : {shpCode} / shpVal : {shpVal} / shpName : {shpName} / shpGeoCen : {shpGeoCen}')
+            #
 
 
             # xlsxDataL2['dtDate'].apply(lambda x: x.timestamp())
@@ -351,48 +475,35 @@ class DtaProcess(object):
             # 2023.0, 51759.392
 
 
-            inpFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, '*/gpw_v4_national_identifier_grid_rev11_30_sec.tif')
-            fileList = sorted(glob.glob(inpFile))
-            # if (len(fileList) < 1): continue
-            fileInfo = fileList[0]
-            gpwNatData = xr.open_rasterio(fileInfo)
-            gpwNatDataL1 = gpwNatData.sel(band=1)
-            gpwNatDataL2 = gpwNatDataL1.interp({'x': lonList, 'y': latList}, method='nearest')
-            # gpwNatDataL2 = gpwNatDataL1.isel(x=slice(14000, 14100), y=slice(100, 200))
-            # gpwNatDataL2 = gpwNatDataL1.sel(x=slice(119.999, 121.001), y=slice(88.999, 90.001))
-            # gpwNatDataL1['x'].values
+
+
+            # lon1D = gpwNatDataL3['x'].values
+            # lat1D = gpwNatDataL3['y'].values
             #
-            # gpwNatDataL1['x'].values
+            # data = xr.Dataset(
+            #     {
+            #         'NAT': (('time', 'lat', 'lon'), (gpwNatDataL3.values).reshape(1, len(lat1D), len(lon1D)))
+            #         , 'POP': (('time', 'lat', 'lon'), (gpwPopDataL3.values).reshape(1, len(lat1D), len(lon1D)))
+            #     }
+            #     , coords={
+            #         'time': pd.date_range(pd.to_datetime(2020, format='%Y'), periods=1)
+            #         , 'lat': lat1D
+            #         , 'lon': lon1D
+            #     }
+            # )
 
-            # gpwNatDataL2.plot()
+            # print(data)
+
+            # data['NAT'].plot()
+            # plt.show()
+            #
+            # data['POP'].plot()
             # plt.show()
 
-            # gpwNatDataL2.plot()
-            # plt.show()
-            # gpwNatDataL1['x'].values
-
-            # gpwNatDataL1.plot()
-
-
-                # dataL1 = data.to_dataset('band').rename({1: 'val'})
-
-            inpFile = '{}/{}/{}'.format(globalVar['inpPath'], serviceName, '*/gpw_v4_population_count_rev11_2000_30_sec.tif')
-            fileList = sorted(glob.glob(inpFile))
-
-            fileInfo = fileList[0]
-            gpwPopData = xr.open_rasterio(fileInfo)
-            gpwPopDataL1 = gpwPopData.sel(band=1)
-            # gpwPopDataL2 = gpwPopDataL1.sel(x=slice(120, 150), y=slice(40, 50))
-            gpwPopDataL2 = gpwPopDataL1.interp({'x': lonList, 'y': latList}, method='nearest')
-
-            # gpwPopDataL2 = gpwPopDataL1.isel(x=slice(120, 150), y=slice(40, 50))
-
-            # gpwNatDataL1 = gpwNatData.to_dataset(name='NAT')
-            # gpwPopDataL1 = gpwPopData.to_dataset(name='POP')
-            gpwNatDataL1 = gpwNatData.to_dataset(name='NAT').isel(x=slice(120, 150), y=slice(40, 50))
-            gpwPopDataL1 = gpwPopData.to_dataset(name='POP').isel(x=slice(120, 150), y=slice(40, 50))
-
-            gpwData = xr.merge([gpwNatDataL1, gpwPopDataL1])
+            # if (len(dataL5) < 1):
+            #     dataL5 = dataL4
+            # else:
+            #     dataL5 = xr.concat([dataL5, dataL4], "time")
 
             # gpwDataL1 = gpwData
             # gpwDataL1['NAT'] = xr.where((gpwDataL1['NAT'] == gpwNatData.nodatavals), np.nan, gpwDataL1['NAT'])
@@ -410,37 +521,24 @@ class DtaProcess(object):
             # 결측값 처리
             # dataL3 = xr.where((dataL3 < 0), np.nan, dataL3)
 
-            gpwNatDataL3 = xr.where((gpwNatDataL2 == gpwNatDataL2.nodatavals), np.nan, gpwNatDataL2)
-            gpwPopDataL3 = xr.where((gpwPopDataL2 == gpwPopDataL2.nodatavals), np.nan, gpwPopDataL2)
+            # gpwNatDataL1 = gpwNatData.sel(band=1)
+            # gpwNatDataL2 = gpwNatDataL1.interp({'x': lonList, 'y': latList}, method='nearest')
+            # gpwNatDataL2 = gpwNatDataL1.isel(x=slice(14000, 14100), y=slice(100, 200))
+            # gpwNatDataL2 = gpwNatDataL1.sel(x=slice(119.999, 121.001), y=slice(88.999, 90.001))
+            # gpwNatDataL1['x'].values
+            #
+            # gpwNatDataL1['x'].values
 
+            # gpwNatDataL2.plot()
+            # plt.show()
 
-            lon1D = gpwNatDataL3['x'].values
-            lat1D = gpwNatDataL3['y'].values
+            # gpwNatDataL2.plot()
+            # plt.show()
+            # gpwNatDataL1['x'].values
 
-            data = xr.Dataset(
-                {
-                    'NAT': (('time', 'lat', 'lon'), (gpwNatDataL3.values).reshape(1, len(lat1D), len(lon1D)))
-                    , 'POP': (('time', 'lat', 'lon'), (gpwPopDataL3.values).reshape(1, len(lat1D), len(lon1D)))
-                }
-                , coords={
-                    'time': pd.date_range(pd.to_datetime(2020, format='%Y'), periods=1)
-                    , 'lat': lat1D
-                    , 'lon': lon1D
-                }
-            )
+            # gpwNatDataL1.plot()
 
-            # print(data)
-
-            data['NAT'].plot()
-            plt.show()
-
-            data['POP'].plot()
-            plt.show()
-
-            # if (len(dataL5) < 1):
-            #     dataL5 = dataL4
-            # else:
-            #     dataL5 = xr.concat([dataL5, dataL4], "time")
+            # dataL1 = data.to_dataset('band').rename({1: 'val'})
 
 
         except Exception as e:
