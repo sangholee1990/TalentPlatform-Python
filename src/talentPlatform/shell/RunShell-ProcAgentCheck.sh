664cf0a3ebc99e7c7d004f1ac9a60f6801b4d23c
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2025-07-14 01:25:13 +0900

    Merge remote-tracking branch 'origin/master'

--- src/talentPlatform/shell/RunShell-ProcAgentCheck.sh
+++ src/talentPlatform/shell/RunShell-ProcAgentCheck.sh
@@ -11,6 +11,10 @@ echo
 # pkill -f SOLARMY_APP && cd /SYSTEMS/IOT/Roverdyn/daemon-iot-tcpipDb/build && cmake .. && make && bash /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/shell/RunShell-ProcAgentCheck.sh
 # tail -f /SYSTEMS/PROG/SHELL/PROC/LOG/*.log
 
+# pkill -f SOLARMY_APP
+# pkill -f TCP-Server-Bees
+# pkill -f TalentPlatform-bdwide-FileWatch
+
 #========================================
 # Set Env
 #========================================
@@ -25,27 +29,27 @@ CTX_PATH=/HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/shell
 
 declare -A metaData
 #metaData["SOLARMY_APP"]="/SYSTEMS/IOT/Roverdyn/PROJ_TCP_DB/build/SOLARMY_APP"
-metaData["SOLARMY_APP"]="/SYSTEMS/IOT/Roverdyn/daemon-iot-tcpipDb/build/SOLARMY_APP"
-metaData["TCP-Server-Bees"]="/SYSTEMS/IOT/Roverdyn/bee-iot/TCP-Server-Bees"
 #metaData["TalentPlatform-LSH0605-DaemonFramework.py"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py38/bin/python /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/unitSys/TalentPlatform-LSH0605-DaemonFramework.py"
 #metaData["TalentPlatform-LSH0613-DaemonFramework-Active-OpenAPI-DataCollectToPrep.py"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py38/bin/python /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/unitSys/TalentPlatform-LSH0613-DaemonFramework-Active-OpenAPI-DataCollectToPrep.py"
 #metaData["TalentPlatform-LSH0613-DaemonFramework-Active-OpenAPI-Model.py"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py36/bin/python /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/unitSys/TalentPlatform-LSH0613-DaemonFramework-Active-OpenAPI-Model.py"
 #metaData["open-webui"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py311/bin/open-webui serve --host 0.0.0.0 --port 9310"
 #metaData["open-webui"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py311/bin/open-webui serve --host 0.0.0.0 --port 9210"
 #metaData["port 9099"]="bash /DATA/TMP/pipelines/start.sh"
+metaData["SOLARMY_APP"]="/SYSTEMS/IOT/Roverdyn/daemon-iot-tcpipDb/build/SOLARMY_APP"
+metaData["TCP-Server-Bees"]="/SYSTEMS/IOT/Roverdyn/bee-iot/TCP-Server-Bees"
 
 metaData["open-webui"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py311/bin/open-webui serve --host 0.0.0.0 --port 9210"
-metaData["pipelines/start"]="bash /HDD/SYSTEMS/PROG/PYTHON/IDE/src/proj/topbds/2025/pipelines/start.sh"
+metaData["pipelines-start"]="bash /HDD/SYSTEMS/PROG/PYTHON/IDE/src/proj/topbds/2025/pipelines/start.sh"
 metaData["TalentPlatform-LSH0577-DaemonApi"]="cd /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/api && /HDD/SYSTEMS/LIB/anaconda3/envs/py39/bin/python -m uvicorn TalentPlatform-LSH0577-DaemonApi:app --reload --host=0.0.0.0 --port=9000"
 metaData["TalentPlatform-LSH0578-DaemonApi"]="cd /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/api && /HDD/SYSTEMS/LIB/anaconda3/envs/py39/bin/python -m uvicorn TalentPlatform-LSH0578-DaemonApi:app --reload --host=0.0.0.0 --port=9200"
 metaData["TalentPlatform-LSH0597-DaemonApi"]="cd /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/api && /HDD/SYSTEMS/LIB/anaconda3/envs/py39/bin/python -m uvicorn TalentPlatform-LSH0597-DaemonApi:app --reload --host=0.0.0.0 --port=9300"
-metaData["rcmdapt_ai_server"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py38/bin/python /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/rcmdapt/rcmdapt_ai_server.py"
+metaData["rcmdapt-ai-server"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py38/bin/python /HDD/SYSTEMS/PROG/PYTHON/IDE/src/talentPlatform/rcmdapt/rcmdapt_ai_server.py"
 metaData["TalentPlatform-bdwide-FileWatch"]="/HDD/SYSTEMS/LIB/anaconda3/envs/py38/bin/python /SYSTEMS/PROG/PYTHON/IDE/src/proj/bdwide/2025/MNTRG/TalentPlatform-bdwide-FileWatch.py"
 #metaData["rclone mount"]="rclone mount 'gd-bdwide:/DATA/LABEL' '/HDD/DATA/LABEL' --daemon"
 
 LOG_PATH=${CTX_PATH}/LOG
-LOG_NAME=$(basename "$0" .sh)_$(date +"%Y%m%d").log
-mkdir -p ${LOG_PATH}
+#LOG_NAME=$(basename "$0" .sh)_$(date +"%Y%m%d").log
+#mkdir -p ${LOG_PATH}
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] [CHECK] CTX_PATH : $CTX_PATH"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] [CHECK] LOG_PATH : $LOG_PATH"
@@ -59,9 +63,12 @@ for key in "${!metaData[@]}"; do
   #echo "[$(date +"%Y-%m-%d %H:%M:%S")] [CHECK] key : $key / val : $val"
 
   procId=$(pgrep -f "$key")
-#  LOG_NAME=$(basename "$0" .sh)_$(date +"%Y%m%d")_${key}.log
 
-  if [ -z "${procId}" ]; then 
+  LOG_PATH=${CTX_PATH}/LOG/${key}
+  LOG_NAME=$(basename "$0" .sh)_${key}_$(date +"%Y%m%d").log
+  mkdir -p ${LOG_PATH}
+
+  if [ -z "${procId}" ]; then
 #      case "$key" in
 #        "TalentPlatform-LSH0605-DaemonFramework")
 #		      ps -ef | grep "chrome" | awk '{print $2}' | xargs kill -9
@@ -69,6 +76,7 @@ for key in "${!metaData[@]}"; do
 #	      *)
 #		      ;;
 #      esac
+
       echo "[$(date +"%Y-%m-%d %H:%M:%S")] [CHECK] Process is not running : $key"
       echo "[$(date +"%Y-%m-%d %H:%M:%S")] [CHECK] Process is not running : $key" >> ${LOG_PATH}/${LOG_NAME}
 #      nohup $val >> ${LOG_PATH}/${LOG_NAME} 2>&1 &
